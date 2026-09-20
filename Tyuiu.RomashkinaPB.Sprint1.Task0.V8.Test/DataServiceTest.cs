125fbe158e55dc9d5ecebabef808f039b6c73903
polina-romashkina <romaskapola7@gmail.com>
2026-09-21 00:08:25 +0500

    Fix

--- Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Test/DataServiceTest.cs
+++ Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Test/DataServiceTest.cs
@@ -5,6 +5,7 @@ namespace Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Test
     [TestClass]
     public sealed class DataServiceTest
     {
+        [TestMethod]
         public void ValidExpression()
         {
             DataService ds = new DataService();
