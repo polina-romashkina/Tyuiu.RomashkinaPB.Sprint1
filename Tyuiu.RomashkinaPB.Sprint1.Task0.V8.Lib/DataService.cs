e728096a469b694f1c16cf0221a2692174cc6e32
polina-romashkina <romaskapola7@gmail.com>
2026-09-21 00:10:24 +0500

    fix1

--- Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Lib/DataService.cs
+++ Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Lib
     {
         public double Calculate()
         {
-            return 15 / 2 / 4;
+            return 15.0 / 2 / 4;
         }
     }
 }
--- Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Test/DataServiceTest.cs
+++ Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.RomashkinaPB.Sprint1.Task0.V8.Test
         {
             DataService ds = new DataService();
             var res = ds.Calculate();
-            Assert.AreEqual(1, res);
+            Assert.AreEqual(1.875, res);
         }
     }
 }
